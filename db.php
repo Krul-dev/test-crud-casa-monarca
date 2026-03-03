fe075881cae3504ae53087eb3964ee9ebadc731c
Raul Gomez <rgomez@tec.mx>
2026-03-02 22:11:35 -0600

    Setting up Docker

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-$host = getenv('DB_HOST') ?: 'db';
+$host = getenv('DB_HOST') ?: '127.0.0.1';
 $port = getenv('DB_PORT') ?: '3306';
 $dbName = getenv('DB_NAME') ?: 'test_crud';
 $user = getenv('DB_USER') ?: 'crud_user';
