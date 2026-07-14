98e9cfaf846b98df6b042a6744dbacd24a8eaa02
Raul Gomez <rgomez@tec.mx>
2026-07-14 09:29:35 -0600

    Add create table to schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,20 @@
+/*
+ * Database initialization script for the CRUD application.
+ *
+ * Responsibilities:
+ * - Create the application database if it does not exist.
+ * - Create the tables required by the application.
+ */
+
+-- Create the application database.
+CREATE DATABASE IF NOT EXISTS app
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+-- Select the application database.
 USE app;
 
+-- Create the users table.
 CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(120) NOT NULL,
